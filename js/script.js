0de3db5c539c62c36a6e67036f68db4fc5cf4416
MariaNarchilevich <87192847+MariaNarchilevich@users.noreply.github.com>
2022-01-11 22:22:40 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,32 +5,3 @@ $(document).ready(function(){
         $('body').toggleClass('lock');
     });
 });
-
-//анимация мальчика
-    let pos1 = document.querySelector('.boyflex__b1');
-    let pos2 = document.querySelector('.boyflex__b2');
-    let pos3 = document.querySelector('.boyflex__b3');
-    let kirp = document.querySelector('.boyflex__kirp');
-
-    if(window.matchMedia("(min-width: 767px)").matches)
-    {
-        pos1.addEventListener('click', function () { 
-            pos1.classList.add('d-none');
-            pos2.classList.remove('d-none');
-            kirp.classList.add('kirp-anim');
-        })  
-    }
-
-    pos1.addEventListener('mouseover', function () { 
-        pos1.classList.add('d-none');
-        pos2.classList.remove('d-none');
-        kirp.classList.add('kirp-anim');
-    })
-    pos2.addEventListener('click', function () {
-        pos2.classList.add('d-none'); 
-        pos3.classList.remove('d-none');
-        kirp.classList.add('kirp-anim2');
-        let boy = document.querySelector('.boy-tr');
-        boy.classList.add('boy-animate');
-        kirp.classList.add('kirp-anim2');
-    });
